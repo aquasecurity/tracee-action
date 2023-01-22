44e7e5bda8b1dceabea46f04dbb7a38bf57edb47
Jose Donizetti <jdbjunior@gmail.com>
2023-01-21 21:03:23 -0300

    refactor: rename to signatures.jsonl

--- create-pr-comment.sh
+++ create-pr-comment.sh
@@ -4,12 +4,12 @@ pull_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 comment=$(cat <<EOF
 This PR has triggered the signatures:
 
-$(jq .eventName /tmp/tracee-action/signatures.json | sort | uniq )
+$(jq .eventName /tmp/tracee-action/signatures.jsonl | sort | uniq )
 
 Please review the Tracee [documentation](https://aquasecurity.github.io/tracee/v0.10/docs/detecting/rules/) to understand the signatures, and review the details of each trigger on the json below:
 
 \`\`\`
-$(cat /tmp/tracee-action/signatures.json)
+$(cat /tmp/tracee-action/signatures.jsonl)
 \`\`\`
 EOF
 )
